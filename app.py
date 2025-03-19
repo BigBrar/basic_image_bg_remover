63f94e6052c290ac4d702cae5b0293d96d143139
Deepinder Singh Brar <deepindersinghdeep03@gmail.com>
2025-03-19 21:54:38 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask, render_template, request, send_file
 from rembg import remove
 from io import BytesIO
 import base64
+import os
 
 app = Flask(__name__)
 
@@ -45,4 +46,5 @@ def index():
     return render_template('index.html')
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 8000))
+    app.run(debug=True,host='0.0.0.0',port=port)
